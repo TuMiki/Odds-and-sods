7aff952e807992d1ef8ad7a5319d976014aca3ce
TuMiki <txmiki@gmail.com>
2010-01-11 09:55:57 +0900

    座標表現用のクラスを作成

---
+++ sunflower/coord.cpp
@@ -0,0 +1,116 @@
+#include <math.h>
+
+#include "coord.h"
+
+/*
+ * コンストラクタ
+ */
+coord::coord(int n) {
+  dim = n;
+  v[2] = 0;
+  for(int i=0; i < dim; i++) {
+    v[i] = 0;
+  }
+}
+
+coord::coord(double x, double y) {
+  dim = 2;
+  v[0] = x;
+  v[1] = y;
+  v[2] = 0;
+}
+
+coord::coord(const coord& cod) {
+  dim = cod.dim;
+  v[2] = 0;
+  for(int i=0; i < dim; i++) {
+    v[i] = cod.v[i];
+  }
+}
+
+/*
+ * デストラクタ
+ */
+coord::~coord() {
+  // 今のところ必要ない
+}
+
+coord& coord::operator=(const coord& cod) {
+  dim = cod.dim;
+  v[2] = 0;
+  for(int i=0; i < dim; i++) {
+    v[i] = cod.v[i];
+  }
+
+  return *this;
+}
+
+coord coord::operator+(const coord& cod) {
+  coord tmp(*this);
+  for(int i=0; i < tmp.dim; i++) {
+    tmp.v[i] += cod.v[i];
+  }
+  return tmp;
+}
+
+coord coord::operator-(const coord& cod) {
+  coord tmp(*this);
+  for(int i=0; i < tmp.dim; i++) {
+    tmp.v[i] -= cod.v[i];
+  }
+  return tmp;
+}
+
+coord coord::operator*(double a) {
+  coord tmp(*this);
+  tmp.v[0] *= a;
+  tmp.v[1] *= a;
+  tmp.v[2] *= a;
+  return tmp;
+}
+
+coord coord::operator*(const coord& cod) {
+  // TODO: 双方のベクトルで次元が違うとエラーかな？0解釈？
+  coord tmp(3);
+  tmp.v[0] = this->v[1]*cod.v[2] - this->v[2]*cod.v[1];
+  tmp.v[1] = this->v[2]*cod.v[0] - this->v[0]*cod.v[2];
+  tmp.v[2] = this->v[0]*cod.v[1] - this->v[1]*cod.v[0];
+  return tmp;
+}
+
+std::string coord::toString() {
+  std::string a;
+  char tmp[256];
+
+  a = "(";
+  sprintf(tmp, "%lg", this->v[0]);
+  a += std::string(tmp);
+  a += ",";
+  sprintf(tmp, "%lg", this->v[1]);
+  a += std::string(tmp);
+  if(this->dim==3) {
+    sprintf(tmp, "%lg", this->v[2]);
+    a += std::string(",") + std::string(tmp);
+  }
+  a += ")";
+  return a;
+}
+
+#define sq(x) ((x)*(x))
+
+double coord::length() {
+  return sqrt(sq(this->v[0]) + sq(this->v[1]) + sq(this->v[2]));
+}
+
+double coord::dot(const coord& cod) {
+  return this->v[0] * cod.v[0] + this->v[1] * cod.v[1] + this->v[2] * cod.v[2];
+}
+
+coord operator*(const double a, const coord& b) {
+  coord tmp(b);
+  return tmp * a;
+}
+
+std::basic_ostream<char>& operator<<(std::basic_ostream<char>& o, coord cod) {
+  return o << cod.toString();
+}
---
+++ sunflower/coord.h
@@ -0,0 +1,45 @@
+#ifndef __coord_h
+#define __coord_h
+
+#include <string>
+#include <iostream>
+
+/*
+ * 座標を扱うためのclass.
+ * vectorとしたいところだが、コンテナクラスに同名があるため、coordとした
+ */
+class coord {
+  protected:
+    /* 以下の変数宣言は実装に関しているため隠したいところ。
+     * C++だとヘッダーファイルに書いたクラス定義が公開となるので通常だと隠せない
+     * 操作・参照はできないのだけど、実装の変更が変数に及んだときに
+     * ヘッダーファイルを変更するのは、気持ちが悪い
+     */
+    int dim;
+    double v[3];
+
+  public:
+    coord(int dim); // 指定した次元で座標を生成
+    coord(double x, double y);  // 2次元で座標を生成
+    coord(const coord& cod);  // 指定した座標をコピーして生成
+    ~coord();  // デストラクタ
+
+    coord& operator=(const coord& cod); // coord = coord。戻り値が参照！
+    coord operator+(const coord& cod); // coord + coord
+    coord operator-(const coord& cod); // coord - coord
+    coord operator*(double a); // coord * double
+    coord operator*(const coord& cod); // 外積
+
+    double length();
+    double dot(const coord& cod); // 内積
+
+    std::string toString();
+};
+
+// 2項演算子で左側が coord 以外の場合は、通常の関数として演算子をオーバーロードする
+// double * coord を解釈する
+coord operator*(const double a, const coord& b);
+
+std::basic_ostream<char>& operator<<(std::basic_ostream<char>& o, coord cod);
+
+#endif
